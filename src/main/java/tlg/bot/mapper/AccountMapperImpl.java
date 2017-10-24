856aac48fb53ee4f913c1c1cb56cbd0c5be5871d
yh8i9kb8n <krivolvlad@gmail.com>
2017-10-24 15:21:00 +0300

    refactored mappers

--- src/main/java/tlg/bot/mapper/AccountMapperImpl.java
+++ src/main/java/tlg/bot/mapper/AccountMapperImpl.java
@@ -10,51 +10,68 @@ import java.util.List;
 
 public class AccountMapperImpl implements AccountMapper{
 
-    private SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
-    private AccountMapper accountMapper = sqlSession.getMapper(AccountMapper.class);
+
 
     @Override
     public void insertAccount(Account account) {
+        SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+        AccountMapper accountMapper = sqlSession.getMapper(AccountMapper.class);
         accountMapper.insertAccount(account);
         sqlSession.commit();
+        sqlSession.close();
     }
 
     @Override
     public Account findByTelegramID(Integer telegramID) {
+        SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+        AccountMapper accountMapper = sqlSession.getMapper(AccountMapper.class);
         Account account = accountMapper.findByTelegramID(telegramID);
-        sqlSession.commit();
+        sqlSession.close();
         return account;
     }
 
     @Override
     public List<Account> findAllAccounts() {
+        SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+        AccountMapper accountMapper = sqlSession.getMapper(AccountMapper.class);
         List<Account> accounts = accountMapper.findAllAccounts();
-        sqlSession.commit();
+        sqlSession.close();
         return accounts;
     }
 
     @Override
     public Account findById(Integer id) {
+        SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+        AccountMapper accountMapper = sqlSession.getMapper(AccountMapper.class);
         Account account = accountMapper.findById(id);
-        sqlSession.commit();
+        sqlSession.close();
         return account;
     }
 
     @Override
     public void delete(Integer id) {
+        SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+        AccountMapper accountMapper = sqlSession.getMapper(AccountMapper.class);
         accountMapper.delete(id);
         sqlSession.commit();
+        sqlSession.close();
     }
 
     @Override
     public void deleteByTelegramId(Integer id) {
+        SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+        AccountMapper accountMapper = sqlSession.getMapper(AccountMapper.class);
         accountMapper.deleteByTelegramId(id);
         sqlSession.commit();
+        sqlSession.close();
     }
 
     @Override
     public void update(Account account) {
+        SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+        AccountMapper accountMapper = sqlSession.getMapper(AccountMapper.class);
         accountMapper.update(account);
         sqlSession.commit();
+        sqlSession.close();
     }
 }
--- src/main/java/tlg/bot/mapper/RequestMapperImpl.java
+++ src/main/java/tlg/bot/mapper/RequestMapperImpl.java
@@ -9,51 +9,66 @@ import java.util.List;
 
 public class RequestMapperImpl implements RequestMapper {
 
-    private static SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
-    private static RequestMapper requestMapper = sqlSession.getMapper(RequestMapper.class);
-
     @Override
     public void insertRequest(Request request) {
+        SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+        RequestMapper requestMapper = sqlSession.getMapper(RequestMapper.class);
         requestMapper.insertRequest(request);
         sqlSession.commit();
+        sqlSession.close();
     }
 
     @Override
     public List<Request> selectRequestsByAccountId(Integer id) {
+        SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+        RequestMapper requestMapper = sqlSession.getMapper(RequestMapper.class);
         List<Request> requests = requestMapper.selectRequestsByAccountId(id);
-        sqlSession.commit();
+        sqlSession.close();
         return requests;
     }
 
     @Override
     public List<Request> selectTop10RequestsByAccountId(Integer id) {
+        SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+        RequestMapper requestMapper = sqlSession.getMapper(RequestMapper.class);
         List<Request> requests = requestMapper.selectTop10RequestsByAccountId(id);
-        sqlSession.commit();
+        sqlSession.close();
         return requests;
     }
 
     @Override
     public List<Request> findAllRequests() {
+        SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+        RequestMapper requestMapper = sqlSession.getMapper(RequestMapper.class);
         List<Request> requests = requestMapper.findAllRequests();
-        sqlSession.commit();
+        sqlSession.close();
         return requests;
     }
 
     @Override
     public void update(Request request) {
+        SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+        RequestMapper requestMapper = sqlSession.getMapper(RequestMapper.class);
         requestMapper.update(request);
         sqlSession.commit();
+        sqlSession.close();
     }
 
     @Override
     public void deleteById(Integer id) {
+        SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+        RequestMapper requestMapper = sqlSession.getMapper(RequestMapper.class);
         requestMapper.deleteById(id);
         sqlSession.commit();
+        sqlSession.close();
     }
 
     @Override
     public void deleteByTelegramId(Integer telegram_id) {
+        SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+        RequestMapper requestMapper = sqlSession.getMapper(RequestMapper.class);
         requestMapper.deleteByTelegramId(telegram_id);
         sqlSession.commit();
+        sqlSession.close();
     }
 }
